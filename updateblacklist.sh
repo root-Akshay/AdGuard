1f7e49f1812dfa51956e810f8373723db306bb9e
Akshay Kadav <root-Akshay@users.noreply.github.com>
2020-11-02 01:17:24 +0530

    Auto Updates Blacklists.

---
+++ updateblacklist.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+url1="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt"
+url2="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt"
+echo "Starting Download"
+sudo wget -qO- $url1 > Blacklists/domains.txt
+echo "Domains Downloaded"
+sudo wget -qO- $url2 > Blacklists/hostnames.txt
+echo "Hostnames Downloaded"
+sudo service dnsmasq restart
+echo "Complete"
