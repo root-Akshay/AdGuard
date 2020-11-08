4d43b586cbfe126dfd8baa2f05238e4734c487c6
Akshay Kadav <root-Akshay@users.noreply.github.com>
2020-11-09 01:14:04 +0530

    Updates Blacklists

--- updateblacklist.sh
+++ updateblacklist.sh
@@ -2,7 +2,6 @@
 
 url1="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt"
 url2="https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt"
-echo "Starting Download"
 sudo wget -qO- $url1 > Blacklists/domains.txt
 echo "Domains Downloaded"
 sudo wget -qO- $url2 > Blacklists/hostnames.txt
